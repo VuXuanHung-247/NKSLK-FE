3e27c747a08a266122541e8d965698fbd22df9db
Vu Xuan Hung <hung.vu@homedy.com>
2021-10-31 14:32:24 +0700

    demo modul phongban

--- ui-vue/src/components/views/Employees/Departments.vue
+++ ui-vue/src/components/views/Employees/Departments.vue
@@ -1,36 +1,51 @@
 <template>
   <section class="content">
     <div class="row center-block">
-      <h2>Quản lý Phòng ban</h2>
       <div class="col-md-12">
         <div class="box">
           <div class="box-header">
-            <h3 class="box-title">Data Table With Full Features</h3>
+            <button type="button" @click="addClick()" class="btn btn-primary" data-toggle="modal" data-target="#dialogModal">
+              Thêm phòng ban
+            </button>
           </div>
           <!-- /.box-header -->
           <div class="box-body">
-            <div class="dataTables_wrapper form-inline dt-bootstrap" id="example1_wrapper">
+            <div
+              class="dataTables_wrapper form-inline dt-bootstrap"
+              id="example1_wrapper"
+            >
               <div class="row">
                 <div class="col-sm-6">
-                  <div id="example1_length" class="dataTables_length">
-                  </div>
+                  <div id="example1_length" class="dataTables_length"></div>
                 </div>
               </div>
 
               <div class="row">
                 <div class="col-sm-12 table-responsive">
-                  <table aria-describedby="example1_info" role="grid" id="example1" class="table table-bordered table-striped dataTable">
+                  <table
+                    aria-describedby="example1_info"
+                    role="grid"
+                    id="example1"
+                    class="table table-bordered table-striped dataTable"
+                  >
                     <thead>
                       <tr role="row">
-                        <th aria-label="Mã phòng ban: activate to sort column ascending" style="width: 101px;" colspan="1" rowspan="1" aria-controls="example1" tabindex="0" class="sorting">Mã phòng ban</th>
+                        <th>
+                          Mã phòng ban
+                        </th>
                         <th>Tên phòng ban</th>
                         <th>Thao tác</th>
                       </tr>
                     </thead>
                     <tbody>
-                      <tr v-for="pb in phongbans" :key="pb" class="even" role="row">
-                        <td class="sorting_1">{{pb.ma_phongban}}</td>
-                        <td class="sorting_1">{{pb.ten_phongban}}</td>
+                      <tr
+                        v-for="dep in departments" :key="dep" class="even" role="row">
+                        <td class="sorting_1">{{ dep.ma_phongban }}</td>
+                        <td class="sorting_1">{{ dep.ten_phongban }}</td>
+                        <td>
+                          <i class="fa fa-pencil-square-o" data-toggle="modal" data-target="#dialogModal" @click="editClick(dep)" style="font-size: 18px;"></i>
+                          <i class="fa fa-trash-o" @click="deleteClick(dep.ma_phongban)"  style="margin-left:20px;font-size: 18px;"></i>
+                        </td>
                       </tr>
                     </tbody>
                   </table>
@@ -40,34 +55,102 @@
             <!-- /.box-body -->
           </div>
         </div>
+        <div class="modal fade" id="dialogModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">{{modalTitle}}</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                Tên phòng ban:
+                <input type="text" class="form-control" v-model="ten_phongban" placeholder="" style="border-radius:3px">
+              </div>
+              <div class="modal-footer">
+                <button type="button" v-if="ma_phongban ==0"  @click="createClick()" class="btn btn-primary">Thêm mới</button>
+                <button type="button" v-if="ma_phongban !=0"  @click="updateClick()" class="btn btn-primary">Sửa</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Hủy</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import axios from 'axios'
+/* eslint-disable */
+import axios from "axios";
 export default {
-  name: 'Departments',
+  name: "Departments",
   data() {
     return {
-      phongbans: [],
+      departments: [],
       ma_phongban: 0,
-      ten_phongban: ''
-    }
+      ten_phongban: "",
+      modalTitle:""
+    };
   },
   methods: {
     refreshData() {
-      axios.get('http://localhost:43932/api/phongban')
-            .then((response) => {
-              this.phongbans = response.data
+      axios.get("http://localhost:43932/api/phongban").then((response) => {
+        this.departments = response.data;
+      });
+    },
+    addClick(){
+            this.modalTitle="Thêm phòng ban";
+            this.ma_phongban=0;
+            this.ten_phongban="";
+        },
+    editClick(dep){
+            this.modalTitle="Sửa phòng ban";
+            this.ma_phongban=dep.ma_phongban;
+            this.ten_phongban=dep.ten_phongban;
+        },
+    createClick() {
+      axios
+        .post("http://localhost:43932/api/phongban", {
+          ten_phongban: this.ten_phongban,
+        })
+        .then((response) => {
+          this.refreshData();
+          alert(response.data);
+        });
+    },
+    updateClick(){
+            axios.put("http://localhost:43932/api/phongban",{
+                ma_phongban:this.ma_phongban,
+                ten_phongban:this.ten_phongban
             })
-    }
+            .then((response)=>{
+                this.refreshData();
+                alert(response.data);
+            });
+    },
+    deleteClick(id){
+            if(!confirm("Bạn chắc chắn muốn xóa phòng ban này?")){
+                return;
+            }
+            axios.delete("http://localhost:43932/api/phongban/"+id)
+            .then((response)=>{
+                this.refreshData();
+                alert(response.data);
+            })
+            .catch(error => {
+                console.log(error)
+                alert("Phòng ban này còn công nhân hoạt động. Không được xóa !");
+                this.errored = true
+              });
+    
+    },
   },
   mounted() {
-    this.refreshData()
-  }
-}
+    this.refreshData();
+  },
+};
 </script>
 
 <style>
@@ -80,23 +163,23 @@ export default {
 @import url('/static/js/plugins/datatables/jquery.dataTables.min.css');
 */
 
-@import url('/static/js/plugins/datatables/dataTables.bootstrap.css');
+@import url("/static/js/plugins/datatables/dataTables.bootstrap.css");
 
 table.dataTable thead .sorting:after,
 table.dataTable thead .sorting_asc:after,
 table.dataTable thead .sorting_desc:after {
-  font-family: 'FontAwesome';
+  font-family: "FontAwesome";
 }
 
 table.dataTable thead .sorting:after {
-  content: '\f0dc';
+  content: "\f0dc";
 }
 
 table.dataTable thead .sorting_asc:after {
-  content: '\f0dd';
+  content: "\f0dd";
 }
 
 table.dataTable thead .sorting_desc:after {
-  content: '\f0de';
+  content: "\f0de";
 }
 </style>
